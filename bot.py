be8d3463f83fa3963d7dcad841c27ad5844c092f
ytfacts96-coder <ytfacts96@gmail.com>
2025-09-10 10:42:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
@@ -30,7 +26,3 @@ class Bot(Client):
         print('Bot Stopped Bye')
 
 Bot().run()
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
