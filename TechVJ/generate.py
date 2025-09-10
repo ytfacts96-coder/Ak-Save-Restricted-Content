88e24d24920a5988245402130476b2ef22e69e40
ytfacts96-coder <ytfacts96@gmail.com>
2025-09-10 10:44:02 +0530

    Update generate.py

--- TechVJ/generate.py
+++ TechVJ/generate.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import traceback
 from pyrogram.types import Message
 from pyrogram import Client, filters
@@ -82,8 +78,3 @@ async def main(bot: Client, message: Message):
     except Exception as e:
         return await message.reply_text(f"<b>ERROR IN LOGIN:</b> `{e}`")
     await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout first and /login again</b>")
-
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
