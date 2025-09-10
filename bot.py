bbc78fb6d57fefc914fe73d874355d59a43e952f
ytfacts96-coder <ytfacts96@gmail.com>
2025-09-10 11:29:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "techvj login",
+            "aryan login",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
