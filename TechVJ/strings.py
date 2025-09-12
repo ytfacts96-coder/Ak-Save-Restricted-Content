d68359afeddefff70b33ac70d43d08d05a405509
ytfacts96-coder <ytfacts96@gmail.com>
2025-09-12 23:32:05 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 HELP_TXT = """**🌟 Help Menu** 
 
 **__FOR PRIVATE CHATS__**
