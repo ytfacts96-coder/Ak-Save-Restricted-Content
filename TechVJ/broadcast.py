6c1b5a33fac96c0c1f4e0d78e2c3b17911917a45
ytfacts96-coder <ytfacts96@gmail.com>
2025-12-12 19:54:59 +0530

    Update broadcast.py

--- TechVJ/broadcast.py
+++ TechVJ/broadcast.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from database.db import db
 from pyrogram import Client, filters
