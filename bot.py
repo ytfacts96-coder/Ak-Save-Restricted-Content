d897c927fc9fce48e1670b8080796e9b9005931e
ytfacts96-coder <ytfacts96@gmail.com>
2025-12-12 19:53:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
 
@@ -38,7 +34,3 @@ class Bot(Client):
 if __name__ == "__main__":
     bot = Bot()
     bot.run()
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
